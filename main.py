c80a6c81b3bb256bf607dcc5884850f4019e0674
powerhorang2 <tjrdbqls3@naver.com>
2022-06-08 11:01:50 +0900

    간단한 다익스트라 알고리즘 소스코드

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# 샘플 Python 스크립트입니다.
+import sys
 
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+input = sys.stdin.readline
+INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수, 간선의 개수 입력받기
+n, m = map(int, input().split())
+# 시작 노드 번호를 입력받기
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n + 1)]
+# 방문한 적이 이는지 체크하는 목적의 리스트 만들기
+visited = [False] * (n + 1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    graph[a].append((b, c))
 
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
+def get_smallest_node():
+    min_value = INF
+    index = 0  # 가장 최단 거리가 짧은 노드(인덱스)
+    for i in range(1, n + 1):
+        if not visited[i] and distance[i] < min_value:
+            min_value = distance[i]
+            index = i
+    return index
 
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def dijkstra(start):
+    # 시작 노드에 대해서 초기화
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
+    for i in range(n - 1):
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
+        now = get_smallest_node()
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
 
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+
+# 다익스트라 알고리즘 수행
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+    if distance[i] == INF:
+        print(i, " = ", "INFINITY")
+    # 도달할 수 있는 경우 거리를 출력
+    else:
+        print(i, " = ", distance[i])
