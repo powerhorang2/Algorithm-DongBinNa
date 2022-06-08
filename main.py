5c2359380c19c70afda577fdca6adfd841d7870d
powerhorang2 <tjrdbqls3@naver.com>
2022-06-08 17:25:54 +0900

    개선된 다익스트라 알고리즘 소스코드

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import heapq
 import sys
 
 input = sys.stdin.readline
@@ -9,8 +10,6 @@ n, m = map(int, input().split())
 start = int(input())
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
 graph = [[] for i in range(n + 1)]
-# 방문한 적이 이는지 체크하는 목적의 리스트 만들기
-visited = [False] * (n + 1)
 # 최단 거리 테이블을 모두 무한으로 초기화
 distance = [INF] * (n + 1)
 
@@ -21,34 +20,25 @@ for _ in range(m):
     graph[a].append((b, c))
 
 
-# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
-def get_smallest_node():
-    min_value = INF
-    index = 0  # 가장 최단 거리가 짧은 노드(인덱스)
-    for i in range(1, n + 1):
-        if not visited[i] and distance[i] < min_value:
-            min_value = distance[i]
-            index = i
-    return index
-
-
 def dijkstra(start):
-    # 시작 노드에 대해서 초기화
+    q = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하며, 큐에 삽입
+    heapq.heappush(q, (0, start))
     distance[start] = 0
-    visited[start] = True
-    for j in graph[start]:
-        distance[j[0]] = j[1]
-    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
-    for i in range(n - 1):
-        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
-        now = get_smallest_node()
-        visited[now] = True
-        # 현재 노드와 연결된 다른 노드를 확인
-        for j in graph[now]:
-            cost = distance[now] + j[1]
-            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[j[0]]:
-                distance[j[0]] = cost
+    while q:    # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 현재 노드의 최단거리가 큐에 저장된 노드의 거리보다 짧을 때 (이미 처리된 적이 있는 노드) 무시
+        # 조건문이 작거나 같다가 안되는 이유는 시작노드의 최단거리를 비교할 때 조건문에 걸림
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
 
 # 다익스트라 알고리즘 수행
