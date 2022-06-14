afc1ad0b2e3f292c973011c4479882d78649fc16
powerhorang2 <tjrdbqls3@naver.com>
2022-06-14 16:15:39 +0900

    구현 - 상하좌우 - 풀이

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-n, k = map(int, input().split())
-count = 0
+n = int(input())
+plans = input().split()
+x, y = 1, 1
 
-while True:
-    # (N == K로 나누어떨어지는 수)가 될 때까지 1씩 빼기
-    target = (n // k) * k
-    count += (n - target)
-    n = target
-    print("count: ", count)
-    print("n: ", n)
-    # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
-    if n < k:
-        print("break")
-        break
-    # K로 나누기
-    count += 1
-    n //= k
-    print("count: ", count)
-    print("n: ", n)
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+direction = ["L", "R", "U", "D"]
 
-count += (n - 1)
-print("end: ", count)
+for plan in plans:
+    for i in range(len(direction)):
+        if plan == direction[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+
+print(x, y)
