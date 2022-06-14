5a6a1144052c002334ff60260e26e71c6b239d29
powerhorang2 <tjrdbqls3@naver.com>
2022-06-14 14:06:40 +0900

    그리디 - 1이 될 때까지 - 풀이 2

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 n, k = map(int, input().split())
-
 count = 0
-while True:
-    if n == 1:
-        break
-    elif n % k == 0:
-        n //= k
-    else:
-        n -= 1
-    count += 1
 
-print(count)
+while True:
+    # (N == K로 나누어떨어지는 수)가 될 때까지 1씩 빼기
+    target = (n // k) * k
+    count += (n - target)
+    n = target
+    print("count: ", count)
+    print("n: ", n)
+    # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
+        print("break")
+        break
+    # K로 나누기
+    count += 1
+    n //= k
+    print("count: ", count)
+    print("n: ", n)
+
+count += (n - 1)
+print("end: ", count)
