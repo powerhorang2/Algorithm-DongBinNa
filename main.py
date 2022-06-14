8e6c0eaadb7f7044a8e16d6a6f3e140ca006def8
powerhorang2 <tjrdbqls3@naver.com>
2022-06-14 10:33:27 +0900

    그리디 - 큰 수의 법칙 - 풀이 1

--- main.py
+++ main.py
@@ -1,54 +1,20 @@
-import heapq
-import sys
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort(reverse=True)
+first = data[0]
+second = data[1]
 
-input = sys.stdin.readline
-INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
+result = 0
 
-# 노드의 개수, 간선의 개수 입력받기
-n, m = map(int, input().split())
-# 시작 노드 번호를 입력받기
-start = int(input())
-# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
-graph = [[] for i in range(n + 1)]
-# 최단 거리 테이블을 모두 무한으로 초기화
-distance = [INF] * (n + 1)
+while True:
+    for _ in range(k):
+        if m == 0:
+            break
+        result += first
+        m -= 1
+    if m == 0:
+        break
+    result += second
+    m -= 1
 
-# 모든 간선 정보를 입력받기
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
-    graph[a].append((b, c))
-
-
-def dijkstra(start):
-    q = []
-    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하며, 큐에 삽입
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q:    # 큐가 비어있지 않다면
-        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-        dist, now = heapq.heappop(q)
-        # 현재 노드의 최단거리가 큐에 저장된 노드의 거리보다 짧을 때 (이미 처리된 적이 있는 노드) 무시
-        # 조건문이 작거나 같다가 안되는 이유는 시작노드의 최단거리를 비교할 때 조건문에 걸림
-        if distance[now] < dist:
-            continue
-        # 현재 노드와 연결된 다른 인접한 노드들을 확인
-        for i in graph[now]:
-            cost = dist + i[1]
-            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
-
-# 다익스트라 알고리즘 수행
-dijkstra(start)
-
-# 모든 노드로 가기 위한 최단 거리를 출력
-for i in range(1, n + 1):
-    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
-    if distance[i] == INF:
-        print(i, " = ", "INFINITY")
-    # 도달할 수 있는 경우 거리를 출력
-    else:
-        print(i, " = ", distance[i])
+print(result)
