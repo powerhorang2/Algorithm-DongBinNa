939aa462870a4eb65e367761a62f7f119ca0f57c
powerhorang2 <tjrdbqls3@naver.com>
2022-06-14 16:28:50 +0900

    구현 - 시각 - 풀이

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 n = int(input())
-plans = input().split()
-x, y = 1, 1
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-direction = ["L", "R", "U", "D"]
+count = 0
+for hour in range(n + 1):
+    for minute in range(60):
+        for second in range(60):
+            if '3' in str(hour) + str(minute) + str(second):
+                count += 1
 
-for plan in plans:
-    for i in range(len(direction)):
-        if plan == direction[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    if nx < 1 or ny < 1 or nx > n or ny > n:
-        continue
-    x, y = nx, ny
-
-print(x, y)
+print(count)
