987a915401a6e0889e616cd278c4e2b60ed110cd
powerhorang2 <tjrdbqls3@naver.com>
2022-06-17 15:53:12 +0900

    DFS_BFS - 재귀 함수 예시 (팩토리얼) - 내 풀이

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def recursive_function(i):
-    if i == 100:
-        return
-    print(i, '번째 재귀 함수에서', i + 1, '번째 재귀 함수를 호출합니다.')
-    recursive_function(i + 1)
-    print(i, '번째 재귀 함수를 종료합니다.')
+def fatorial(n):
+    if n == 1:
+        return 1
+    return n * fatorial(n - 1)
 
 
-recursive_function(1)
+print(fatorial(5))
