fc690520a11d34be681aa8b4a17538fdae1d3dc9
powerhorang2 <tjrdbqls3@naver.com>
2022-06-17 16:31:00 +0900

    DFS_BFS - 인접 행렬 방식(그래프) - 예제

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def fatorial(n):
-    if n == 1:
-        return 1
-    return n * fatorial(n - 1)
+INF = 9999999999    # 무한의 비용 선언
 
+# 2차원 리스트를 이용해 인접 행렬 표현
+graph = [
+    [0, 7, 5],
+    [7, 0, INF],
+    [5, INF, 0]
+]
 
-print(fatorial(5))
+print(graph)
