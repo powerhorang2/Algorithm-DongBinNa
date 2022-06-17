6c410fa5c8695b9500ecf333363e92732139bb43
powerhorang2 <tjrdbqls3@naver.com>
2022-06-17 14:12:26 +0900

    구현 - 게임 개발 - 풀이

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
-input_data = input()
-row = int(input_data[1])
-# 문자를 유니코드 정수로 변환하고 문자 'a'의 유니코드 정수를 빼면 a 부터 0 이되는데 거기에 1을 더하는 테크닉 - 그럼 문자 a 는 정수 1이 된다.
-column = int(ord(input_data[0])) - int(ord('a')) + 1
+n, m = map(int, input().split())
+a, b, direction = map(int, input().split())
 
-# 나이트가 이동할 수 있는 8가지 방향 정의
-steps = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+visited = [[0] * m for _ in range(n)]
+visited[a][b] = 1
 
-result = 0
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+steps = [0, 1, 2, 3]
 
-for step in steps:
-    # 이동 후 위치 파악
-    next_row = row + step[0]
-    next_column = column + step[1]
+array = []
+for _ in range(n):
+    array.append(list(map(int, input().split())))
 
-    # 이동 후 나이트의 위치가 8 X 8 정원안이면 카운트 증가
-    if 1 <= next_row <= 8 and 1 <= next_column <= 8:
-        result += 1
 
-print(result)
+def turn_direction():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+count = 1
+turn_time = 0
+
+while True:
+
+    turn_direction()
+    nx = a + dx[direction]
+    ny = b + dy[direction]
+
+    if array[ny][nx] == 0 and visited[nx][ny] == 0:
+        visited[a][b] = 1
+        a, b = nx, ny
+        count += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+
+    if turn_time == 4:
+        nx = a - dx[direction]
+        ny = b - dy[direction]
+        if array[nx][ny] == 1:
+            break
+        else:
+            a = nx
+            b = ny
+            turn_time = 0
+
+print(count)
