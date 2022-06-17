44a6d2ade3399583468445393b34ccefb03e2f9a
powerhorang2 <tjrdbqls3@naver.com>
2022-06-17 10:59:34 +0900

    구현 - 왕실의 나이트 - 풀이

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-data = input()
-x = int(data[1])
-y = data[0]
+input_data = input()
+row = int(input_data[1])
+# 문자를 유니코드 정수로 변환하고 문자 'a'의 유니코드 정수를 빼면 a 부터 0 이되는데 거기에 1을 더하는 테크닉 - 그럼 문자 a 는 정수 1이 된다.
+column = int(ord(input_data[0])) - int(ord('a')) + 1
 
-dy = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-dx = [1, 2, 3, 4, 5, 6, 7, 8]
-
-moves = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
-
-real_x = dx.index(x)
-real_y = dy.index(y)
+# 나이트가 이동할 수 있는 8가지 방향 정의
+steps = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
 
 result = 0
 
-for move in moves:
-    mx = real_x + move[0]
-    my = real_y + move[1]
+for step in steps:
+    # 이동 후 위치 파악
+    next_row = row + step[0]
+    next_column = column + step[1]
 
-    if mx < 0 or my < 0 or mx > 7 or my > 7:
-        continue
-
-    result += 1
+    # 이동 후 나이트의 위치가 8 X 8 정원안이면 카운트 증가
+    if 1 <= next_row <= 8 and 1 <= next_column <= 8:
+        result += 1
 
 print(result)
