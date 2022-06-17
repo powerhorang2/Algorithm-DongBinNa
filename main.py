e839953c8b10e60a037cb6f0d465f97e90d0b45a
powerhorang2 <tjrdbqls3@naver.com>
2022-06-17 15:43:01 +0900

    DFS_BFS - 재귀 함수 - 예제

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-# 파이썬으로 큐를 구현할 때는 collections 모듈에서 제공하는 deque 자료구조를 활용하자
-# deque 는 스택과 큐의 장점을 모두 채택한 것인데 데이터를 넣고 빼는 속도가 리스트 자료형에 비해
-# 효율적이며 queue 라이브러리를 이용하는 것보다 더 간단하다.
-from collections import deque
+def recursive_function():
+    print('재귀 함수를 호출합니다.')
+    recursive_function()
 
-queue = deque()
 
-# 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
-queue.append(5)
-queue.append(2)
-queue.append(3)
-queue.append(7)
-queue.popleft()
-queue.append(1)
-queue.append(4)
-queue.popleft()
-
-print(queue)    # 먼저 들어온 순서대로 출력
-queue.reverse()
-print(queue)  # 나중에 들어온 원소부터 출력
+recursive_function()
