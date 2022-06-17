b789fccb9b6a0e3c7540035a00e19123f950c5a7
powerhorang2 <tjrdbqls3@naver.com>
2022-06-17 15:06:22 +0900

    DFS_BFS - 큐 - 예제

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-# 파이썬은 기본 리스트 자료형에서 append 와 pop 을 이용해서 간단하게 stack 자료구조를 사용할 수 있다.
-stack = []
+# 파이썬으로 큐를 구현할 때는 collections 모듈에서 제공하는 deque 자료구조를 활용하자
+# deque 는 스택과 큐의 장점을 모두 채택한 것인데 데이터를 넣고 빼는 속도가 리스트 자료형에 비해
+# 효율적이며 queue 라이브러리를 이용하는 것보다 더 간단하다.
+from collections import deque
+
+queue = deque()
 
 # 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
-stack.append(5)
-stack.append(2)
-stack.append(3)
-stack.append(7)
-stack.pop()
-stack.append(1)
-stack.append(4)
-stack.pop()
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
 
-print(stack)    # 최하단 원소부터 출력
-print(stack[::-1])  # 최상단 원소부터 출력
+print(queue)    # 먼저 들어온 순서대로 출력
+queue.reverse()
+print(queue)  # 나중에 들어온 원소부터 출력
