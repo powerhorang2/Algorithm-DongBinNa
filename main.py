ec6af38dd1c951c8b9d07c317fabd8d6d05845bb
powerhorang2 <tjrdbqls3@naver.com>
2022-06-17 10:50:38 +0900

    구현 - 왕실의 나이트 - 풀이 - 내 풀이

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-n = int(input())
+data = input()
+x = int(data[1])
+y = data[0]
 
-count = 0
-for hour in range(n + 1):
-    for minute in range(60):
-        for second in range(60):
-            if '3' in str(hour) + str(minute) + str(second):
-                count += 1
+dy = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+dx = [1, 2, 3, 4, 5, 6, 7, 8]
 
-print(count)
+moves = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+
+real_x = dx.index(x)
+real_y = dy.index(y)
+
+result = 0
+
+for move in moves:
+    mx = real_x + move[0]
+    my = real_y + move[1]
+
+    if mx < 0 or my < 0 or mx > 7 or my > 7:
+        continue
+
+    result += 1
+
+print(result)
