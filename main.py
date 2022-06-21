2afa8d758aa68811f488a1ec20107f8f0d9fea87
powerhorang2 <tjrdbqls3@naver.com>
2022-06-21 15:40:11 +0900

    정렬 - 파이썬 정렬 - 정렬 라이브러리에서 Key를 활용한 소스코드

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+array = [('바나나', 2), ('사과', 5), ('당근', 3)]
 
-array.sort()
-print(array)
+
+def setting(data):
+    return data[1]
+
+
+result = sorted(array, key=setting)
+print(result)
