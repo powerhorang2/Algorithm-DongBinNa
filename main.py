49e3d81ecf87159fe6cf329c77b6abe8c134ea00
powerhorang2 <tjrdbqls3@naver.com>
2022-06-21 17:29:56 +0900

    이진 탐색 - 이진 탐색(반복문으로 구현) - 소스코드

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# 이진 탐색 소스코드 구현(재귀 함수)
+# 이진 탐색 소스코드 구현(반복문)
 def binary_search(array, target, start, end):
-    if start > end:
-        return None
-    mid = (start * end) // 2
-    # 찾은 경우 중간점 인덱스 반환
-    if array[mid] == target:
-        return mid
-    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid - 1)
-    # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
-    elif array[mid] < target:
-        return binary_search(array, target, mid + 1, end)
+    while start <= end:
+        mid = (start + end) // 2
+        # 찾은 경우 중간점 인덱스 반환
+        if array[mid] == target:
+            return mid
+        # 중간점의 값보다 찾고자 하는 값이 적은 경우 왼쪽 확인
+        elif array[mid] > target:
+            end = mid - 1
+        # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+        elif array[mid] < target:
+            start = mid + 1
+    return None
 
 
 # n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
