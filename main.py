7ced3f2bd4b3011a2a3bbad0b71a8617d4e1727e
powerhorang2 <tjrdbqls3@naver.com>
2022-06-21 17:43:52 +0900

    효율적인 한 줄 입력 코드 - 소스 코드

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-# 이진 탐색 소스코드 구현(반복문)
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        # 찾은 경우 중간점 인덱스 반환
-        if array[mid] == target:
-            return mid
-        # 중간점의 값보다 찾고자 하는 값이 적은 경우 왼쪽 확인
-        elif array[mid] > target:
-            end = mid - 1
-        # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
-        elif array[mid] < target:
-            start = mid + 1
-    return None
+import sys
+# 하나의 문자열 데이터 입력받기
+input_data = sys.stdin.readline().rstrip()
 
-
-# n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
-n, target = map(int, input().split())
-# 전체 원소 입력받기
-array = list(map(int, input().split()))
-
-# 이진 탐색 수행 결과 출력
-result = binary_search(array, target, 0, n - 1)
-if result == None:
-    print("원소가 존재하지 않습니다.")
-else:
-    print(result + 1)
+# 입력받은 문자열 그대로 출력
+print(input_data)
