37a5929cadfb82ec27b50417ba5bcf328c6654d1
powerhorang2 <tjrdbqls3@naver.com>
2022-06-21 11:41:23 +0900

    정렬 - 삽입 정렬 - 소스코드

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# 0 인덱스와 1 인 인덱스의 원소 스와프
-array = [3, 5]
-array[0], array[1] = array[1], array[0]  # 스와프
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):   # 인덱스 i부터 1까지 감소하며 반복하는 문법
+        if array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+        else:   # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+            break
 
 print(array)
