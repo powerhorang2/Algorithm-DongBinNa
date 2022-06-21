e1236c875703bb06ee53e844c0d1aa63ee9e2e8c
powerhorang2 <tjrdbqls3@naver.com>
2022-06-21 15:36:22 +0900

    정렬 - 파이썬 정렬 - sorted() 함수

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-# 모든 원소의 값이 0보다 크거나 같다고 가정
-array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
-# 모든 범위를 포함하는 리스트 선언(모든 값은 0으로 초기화)
-sort_array = [0] * (max(array) + 1)
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in array:
-    sort_array[i] += 1  # 각 데이터에 해당하는 인덱스 값 증가
-
-for i in range(len(sort_array)):    # 리스트에 기록된 정렬 정보 확인
-    for _ in range(sort_array[i]):
-        print(i, end=" ")   # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
+result = sorted(array)
+print(result)
