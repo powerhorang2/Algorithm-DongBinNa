f1bab34b4435a67d01ee9dd9e39111ea1ae2890a
powerhorang2 <tjrdbqls3@naver.com>
2022-06-21 17:18:21 +0900

    이진 탐색 - 이진 탐색(재귀 함수로 구현) - 소스코드

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-# 순차 탐색 소스코드 구현
-def sequential_search(n, target, array):
-    # 각 원소를 하나씩 확인하며
-    for i in range(n):
-        # 현재의 원소가 찾고자 하는 원소와 동일한 경우
-        if array[i] == target:
-            return i + 1    # 현재 위치 반환(인덱스는 0부터 시작하므로 1 더하기)
+# 이진 탐색 소스코드 구현(재귀 함수)
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start * end) // 2
+    # 찾은 경우 중간점 인덱스 반환
+    if array[mid] == target:
+        return mid
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+    elif array[mid] < target:
+        return binary_search(array, target, mid + 1, end)
 
 
-print("생성할 원소 개수를 입력한 다음 한 칸 띄고 찾을 문자열을 입력하세요.")
-input_data = input().split()
-n = int(input_data[0])  # 원소의 개수
-target = input_data[1]  # 찾고자 하는 문자열
+# n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
+n, target = map(int, input().split())
+# 전체 원소 입력받기
+array = list(map(int, input().split()))
 
-print("앞서 적은 원소 개수만큼 문자열을 입력하세요. 구분은 띄어쓰기 한 칸으로 합니다. ")
-array = input().split()
-
-# 순차 탐색 수행 결과 출력
-print(sequential_search(n, target, array))
+# 이진 탐색 수행 결과 출력
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
