9ecfbb412ee7d47cbc604ef64af913ff098f29b2
powerhorang2 <tjrdbqls3@naver.com>
2022-06-21 16:24:28 +0900

    정렬 - 실전 문제 - 성적이 낮은 순서로 학생 출력하기

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+# N을 입력받기
 n = int(input())
 
+# N명의 학생 정보를 입력받아 리스트에 저장
 array = []
 for _ in range(n):
-    array.append(int(input()))
+    input_data = input().split()
+    # 이름은 문자열 그대로, 점수는 정수형으로 변환하여 저장
+    array.append((input_data[0], int(input_data[1])))
 
-result = sorted(array, reverse=True)
+# 키(Key)를 이용하여, 점수를 기준으로 정렬
+array = sorted(array, key=lambda data: data[1])
 
-for i in result:
-    print(i, end=" ")
+# 정렬이 수행된 결과를 출력
+for student in array:
+    print(student[0], end=" ")
