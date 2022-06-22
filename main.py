d0968620d48988ba8f8dc4632a0368f71f9e28c9
powerhorang2 <tjrdbqls3@naver.com>
2022-06-22 17:12:05 +0900

    이진 탐색 - 실전 문제 - 부품 찾기(집합 자료형 이용)

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 # N(가게의 부품 개수) 입력
 n = int(input())
-array = [0] * 1000001
+# 가게에 있는 전체 부품 번호를 입력받아서 집합(set) 자료형에 기록
+array = set(map(int, input().split()))
 
-# 가게에 있는 전체 부품 번호를 입력받아서 기록
-for i in input().split():
-    array[int(i)] = 1
-    
 # M(손님이 확인 요청한 부품 개수) 입력
 m = int(input())
 # 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
@@ -14,7 +11,7 @@ customer = list(map(int, input().split()))
 # 손님이 확인 요청한 부품 번호를 하나씩 확인
 for i in customer:
     # 해당 부품이 존재하는지 확인
-    if array[i] == 0:
-        print("no", end=" ")
-    elif array[i] == 1:
+    if i in array:
         print("yes", end=" ")
+    else:
+        print("no", end=" ")
