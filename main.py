55d26760f4b7c7665bc53b6050165086216559b3
powerhorang2 <tjrdbqls3@naver.com>
2022-06-22 16:48:58 +0900

    이진 탐색 - 실전 문제 - 부품 찾기(이진 탐색) - 재귀 함수로 이진 탐색 구현

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
+# 이진 탐색 소스코드 구현(재귀 함수)
 def binary_search(array, target, start, end):
+    # 시작 인덱스가 끝 인덱스보다 크다는건 찾아봐도 타겟이 없다는 것
     if start > end:
         return None
     mid = (start + end) // 2
+    # 타겟을 찾은 경우 중간점 인덱스 반환
     if array[mid] == target:
-        return target
+        return mid
+    # 타겟이 중간점의 값보다 작은 경우 왼쪽 확인
     elif array[mid] > target:
         return binary_search(array, target, start, mid - 1)
+    # 타겟이 중간점의 값보다 큰 경우 오른쪽 확인
     elif array[mid] < target:
         return binary_search(array, target, mid + 1, end)
 
 
+# N(가게의 부품 개수) 입력
 n = int(input())
+# 가게에 있는 전체 부품 번호를 공백으로 구분하여 입력
 shop = list(map(int, input().split()))
-shop.sort()
+shop.sort()  # 이진 탐색을 수행하기 위해 사전에 정렬 수행
+# M(손님이 확인 요청한 부품 개수) 입력
 m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
 customer = list(map(int, input().split()))
 
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
 for i in customer:
-    if binary_search(shop, i, 0, len(shop) - 1) is None:
+    # 해당 부품이 존재하는지 확인
+    result = binary_search(shop, i, 0, len(shop) - 1)
+    if result is None:
         print("no", end=" ")
     else:
         print("yes", end=" ")
