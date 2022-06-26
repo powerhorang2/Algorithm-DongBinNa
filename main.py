a3de0b4357eb6f5578e16f50967f84c3c43660cc
powerhorang2 <tjrdbqls3@naver.com>
2022-06-26 13:52:37 +0900

    이진 탐색 - 실전 문제 - 떡볶이 떡 만들기

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-# N(가게의 부품 개수) 입력
-n = int(input())
-# 가게에 있는 전체 부품 번호를 입력받아서 집합(set) 자료형에 기록
-array = set(map(int, input().split()))
+# 떡의 개수(N)와 요청한 떡의 길이(M)을 입력받기
+n, m = map(int, input().split())
+# 각 떡의 개별 높이 정보를 입력받기
+array = list(map(int, input().split()))
 
-# M(손님이 확인 요청한 부품 개수) 입력
-m = int(input())
-# 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
-customer = list(map(int, input().split()))
+# 이진 탐색을 위한 시작점과 끝점 설정
+start = 0
+end = max(array)
 
-# 손님이 확인 요청한 부품 번호를 하나씩 확인
-for i in customer:
-    # 해당 부품이 존재하는지 확인
-    if i in array:
-        print("yes", end=" ")
+# 이진 탐색 수행(반복적)
+result = 0
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+
+    for x in array:
+        # 잘랐을 때의 떡의 양 계산
+        if x > mid:
+            total += x - mid
+
+    # 자른 후 떡의 총합의 양이 부족한 경우 더 많이 자르기(왼쪽 부분 탐색)
+    if total < m:
+        end = mid - 1
+    # 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
     else:
-        print("no", end=" ")
+        result = mid # 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result에 기록
+        start = mid + 1
+
+# 정답 출력
+print(result)
