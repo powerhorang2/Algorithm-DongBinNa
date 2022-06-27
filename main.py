5424983aeb9ff22f047ee360b88c82496d70ff6b
powerhorang2 <tjrdbqls3@naver.com>
2022-06-27 15:24:04 +0900

    다이나믹 프로그래밍 - 피보나치 수열(반복문) - 소스 코드

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 리스트 초기화
 d = [0] * 100
 
+# 첫 번째 피보나치 수와 두 번째 피보나치 수는 1
+d[1] = 1
+d[2] = 1
 
-def fibo(x):
-    print('f(' + str(x) + ')', end=' ')
-    if x == 1 or x == 2:
-        return  1
-    if d[x] != 0:
-        return d[x]
-    d[x] = fibo(x - 1) + fibo(x - 2)
-    return d[x]
+n = 99
 
+# 피보나치 함수(Fibonacci Function) 반복문으로 구현(보텀업 다이나믹 프로그래밍)
+for i in range(3, n + 1):
+    d[i] = d[i - 1] + d[i - 2]
 
-fibo(6)
+print(d[n])
