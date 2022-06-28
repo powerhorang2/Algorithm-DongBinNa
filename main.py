a37fdc7b7950a39650841fea97eedc63922eba6c
powerhorang2 <tjrdbqls3@naver.com>
2022-06-28 17:04:06 +0900

    다이나믹 프로그래밍 - 실전 문제 - 바닥 공사

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 # 정수 N을 입력받기
 n = int(input())
 
-# 모든 식량 정보 입력받기
-array = list(map(int, input().split()))
-
-# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
-d = [0] * 100
+# 앞서 계산된 결과를 지정하기 위한 DP 테이블 초기화
+d = [0] * 1001
 
 # 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
-d[0] = array[0]
-d[1] = max(array[0], array[1])
-for i in range(2, n):
-    d[i] = max(d[i - 1], d[i - 2] + array[i])
+d[1] = 1
+d[2] = 3
+for i in range(3, n + 1):
+    d[i] = (d[i - 1] + 2 * d[i - 2]) % 769769  # 값을 계산할 때마다 나누는 이유는 모듈러 연산을 참고하면 된다.
 
-# 결과 출력
-print(d[n - 1])
+# 계산된 결과를 출력
+print(d[n])
