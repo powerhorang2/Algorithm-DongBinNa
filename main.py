c6b5023d1742a5abe6def4177683619feb277da7
powerhorang2 <tjrdbqls3@naver.com>
2022-06-29 14:39:09 +0900

    최단 거리 - 다익스트라 알고리즘(개선된 버전) - 소스 코드

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import heapq
 import sys
 input = sys.stdin.readline
 INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
@@ -8,8 +9,6 @@ n, m = map(int, input().split())
 start = int(input())
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
 graph = [[] for i in range(n + 1)]
-# 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
-visited = [False] * (n + 1)
 # 최단 거리 테이블을 모두 무한으로 초기화
 distance = [INF] * (n + 1)
 
@@ -20,35 +19,24 @@ for _ in range(m):
     graph[a].append((b, c))
 
 
-# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
-def get_smallest_node():
-    min_value = INF
-    index = 0  # 가장 최단 거리가 짧은 노드 번호(인덱스)
-    for i in range(1, n + 1):
-        if distance[i] < min_value and not visited[i]:
-            min_value = distance[i]
-            index = i
-    return index
-
-
 def dijkstra(start):
-    # 시작 노드에 대해서 초기화
+    queue = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하며, 큐에 삽입
+    heapq.heappush(queue, (0, start))
     distance[start] = 0
-    visited[start] = True
-    for j in graph[start]:
-        distance[j[0]] = j[1]
-    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
-    for _ in range(n - 1):
-        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
-        now = get_smallest_node()
-        visited[now] = True
-        # 현재 노드와 연결된 다른 노드를 확인
-        for j in graph[now]:
-            # distance[j[0]] = min(distance[now] + j[1], distance[j[0]]) # 내 풀이
-            cost = distance[now] + j[1]
-            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[j[0]]:
-                distance[j[0]] = cost
+    while queue:  # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(queue)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(queue, (cost, i[0]))
 
 
 # 다익스트라 알고리즘 수행
