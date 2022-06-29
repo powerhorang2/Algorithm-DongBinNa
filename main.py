3a383c2e1ef37f6a87b05f5994bbd37b71e8a1eb
powerhorang2 <tjrdbqls3@naver.com>
2022-06-29 11:22:38 +0900

    다이나믹 프로그래밍 - 실전 문제 - 효율적인 화폐 구성

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+# 정수 N, M을 입력받기
 n, m = map(int, input().split())
+# N개의 화폐 단위 정보를 입력받기
 array = []
 for _ in range(n):
     array.append(int(input()))
 
-d = [-1] * 10001
+# 한 번 계산된 결과를 저장하기 위한 DP 테이블 초기화
+d = [10001] * (m + 1)
+
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
 d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1):
+        if d[j - array[i]] != 10001:  # (i - k)원을 만드는 방법이 존재하는 경우
+            d[j] = min(d[j], d[j - array[i]] + 1)
 
-start = min(array)
-for money in array:
-    d[money] = 1
-
-for i in range(start, m + 1):
-    for money in array:
-        if d[i - money] > 0 and d[i] > 0:
-            d[i] = min(d[i], d[i - money] + 1)
-        elif d[i - money] > 0 and d[i] < 0:
-            d[i] = d[i - money] + 1
-
-print(d[m])
+# 계산된 결과 출력
+if d[m] == 10001:  # 최종적으로 M원을 만드는 방법이 없는 경우
+    print(-1)
+else:
+    print(d[m])
